cc9b3d9719e67fd66a53c5fb20c414c8c9afebd8
classmatewu <171755422@qq.com>
2023-03-11 08:03:24 -0500

    style: update layout

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // All imports are automatic but if you want to import anything,
 // remember to import from 'vue-termui':
 // import { ref } from 'vue-termui'
-import { getGPTAnswer } from './gpt.ts'
+import { getGPTAnswer } from './gpt'
 
 // 控制动画效果
 const n = ref(0)
@@ -13,11 +13,11 @@ useInterval(() => {
 // 聊天数据
 const input = ref("")
 const chatList = ref([{
-  type: "question",
-  text: "hello"
-}, {
   type: "answer",
-  text: "hi"
+  text: "Hi, I am ChatGPT."
+}, {
+  type: "question",
+  text: "Try press TAB to ask some questions."
 }])
 onKeyData(['Enter'], async () => {
   const question = {
@@ -45,27 +45,46 @@ onKeyData(['d', 'D'], () => {
     :padding="1"
     width="100%"
     :maxWidth="80"
+    :minHeight="18"
     flexDirection="column"
-    borderColor="yellowBright"
+    borderColor="greyBright"
     borderStyle="round"
+    justifyContent="space-between"
+    alignItems="center"
   >
-    <Box 
-      :marginBottom="1" 
-      width="100%"   
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Text color="cyanBright">Hello World </Text>
-      <Text>{{ n % 2 ? '👋 ' : ' ✋' }}</Text>
+    <Box flexDirection="column"     justifyContent="center" width="100%"
+    alignItems="center" :marginBottom="2">
+      <Box :marginBottom="1">
+        <Text color="cyanBright">Welcome </Text>
+        <Text>{{ n % 2 ? '👋 ' : ' ✋' }}</Text>
+      </Box>
+      <Box width="100%" flexDirection="column" :padding="1">
+        <Box v-for="(chat, index) in chatList" :key="index" width="100%" flexDirection="column">
+          <Box v-if="chat.type === 'question'" width="100%" justifyContent="flex-end" alignItems="center">
+            <Box borderColor="greenBright" borderStyle="round" :paddingX="1">
+              <Text>
+                {{chat.text}} 
+              </Text>
+            </Box>
+            <Text color="grey"> :You</Text>
+          </Box>
+          <Box v-if="chat.type === 'answer'" alignItems="center">
+            <Text color="grey">ChatGPT: </Text>
+            <Box borderColor="redBright" borderStyle="round" :paddingX="1">
+              <Text>
+                {{chat.text}}
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Box>
-    <Box v-for="(chat, index) in chatList" :key="index">
-      <Text v-if="chat.type === 'question'">
-        Me: {{chat.text}}
-      </Text>
-      <Text v-if="chat.type === 'answer'">
-        You: {{chat.text}}
-      </Text>
+    <Box alignItems="center">
+      <Input label="" v-model="input" placeholder="Search packages" :disabled="disabled" :marginRight="1" minWidth="60%" />
+      <Text :marginLeft="1" color="grey">sent</Text>
     </Box>
-    <Input label="message: " v-model="input" placeholder="Search packages" :disabled="disabled" />
   </Box>
 </template>
+
+<style lang="scss">
+</style>
